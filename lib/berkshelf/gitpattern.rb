70735c60626d44e8ec93d95d46d37d32c5f5e844
bakins <brian.akins@turner.com>
2013-05-02 14:56:58 -0400

    remove the silly versions tuff

--- lib/berkshelf/gitpattern.rb
+++ lib/berkshelf/gitpattern.rb
@@ -6,11 +6,6 @@ module Berkshelf
     def initialize(name, version_constraint, options = {})
       pattern = options[:gitpattern][:pattern]
       options[:git] = pattern.gsub(/\{NAME\}/, name)
-      @repo_identifier = options.delete(:gitpattern) || name
-      if version_constraint.to_s.match(/^= \d/)
-        tag = version_constraint.to_s.split[1]
-        options[:branch] = options[:branch] || options[:ref] || options[:tag] || tag
-      end
       super
     end
   end
